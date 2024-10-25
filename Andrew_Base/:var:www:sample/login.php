7f075e77c27ea2b842bffb8d2664843b08d52c0a
ab2669 <144724055+ab2669@users.noreply.github.com>
2024-10-24 21:03:07 -0400

    Add files via upload
    Setting up System for Authentication. Only need to edit the sample RabbitMQ configs to connect to our RabbitMQ Config (need to watch Professor's Tutorial again for that). Then next step, is to see if everything works together.

--- Andrew_Base/:var:www:sample/login.php
+++ Andrew_Base/:var:www:sample/login.php
@@ -1,21 +1,54 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Andrew's Sample</title>
+</head>
 
-if (empty($_POST)) {
-    $msg = "NO POST MESSAGE SET, POLITELY F*** OFF";
-    echo json_encode($msg);
-    exit(0);
-}
+<body>
+    <h1>Andrew's Sample</h1>
+    <p>This is a test website to learn how to use Apache2 in my IT490 project.</p>
+    
+    <div id="textResponse">
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+            if (!empty($_POST['username']) && !empty($_POST['password']))
+            {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
 
-$request = $_POST;
-$response = "unsupported request type, politely F*** OFF";
+                $output = [];
+                $return_var = 0;
 
-switch ($request["type"]) {
-    case "login":
-        $response = "login, yeah we can do that";
-        break;
-}
+                //Replace authentication with RabbitMQ
+                exec("php /./ComponentsofLoginPHP/testRabbitMQClient.php $username $password", $output, $return_var);
+                
+                echo implode("\n", $output);
 
-echo json_encode($response);
-exit(0);
+                if ($return_var === 0)
+                {
+                    echo "Login request send seccssfully.";
+                } 
+                else 
+                {
+                    echo "Failed to send login request";
+                }
+            } 
+            else 
+            {
+                echo "Please fill in both username and password.";
+            }
+            //End of authentication section with RabbitMQ (Modify this space, DONT REMOVE THESE COMMENTS)
+        }
+        ?>
+    </div>
 
-?>
+    <!-- Login form -->
+    <form action="login.php" method="POST">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <input type="submit" value="Login">
+    </form>
+
+</body>
+</html>
