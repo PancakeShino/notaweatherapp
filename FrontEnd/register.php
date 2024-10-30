00761871ebb4e5b6023d5e1bb5d7b989075d72ba
PancakeShino <123774288+PancakeShino@users.noreply.github.com>
2024-10-30 00:17:05 -0400

    Update register.php

--- FrontEnd/register.php
+++ FrontEnd/register.php
@@ -6,23 +6,24 @@ require_once 'testRabbitMQClient.php';
 $username = trim($_POST["username"]);
 $password = trim($_POST["password"]);
 
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
 
 $data = [
 	'username' => $username,
 	'password' => $hashed_password,
 ];
 
-$exchange = 'registration_exchange';
-$queue = 'registration_queue';
+$exchange = 'IT490_exchange';
+$queue = 'Registration';
 
 $channel->queue_declare($queue, false, true, false, false);
 $channel->exchange_declare($exchange, 'direct', false, true, false);
 $channel->queue_bind($queue, $exchange);
 
+$msg = new AMQPMessage(json_encode($data), ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]);
+$channel->basic_publish($msg, $exchange);
+
 $channel->close();
 $conn->close();
 
-echo "Registration request sent! Please wait for confirmation. ";
+echo "Registration request sent!";
 ?>
